fca8793ab622b462f7a63e7552075823b83fc4c7
Ana Luisa Oliveira da Silva <analuisaoliveira714@gmail.com>
2022-11-06 20:03:22 -0300

    adicionando a página cadastro

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,12 @@ public class HomeController : Controller
         return View();
     }
 
+
+    public IActionResult Cadastro()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
