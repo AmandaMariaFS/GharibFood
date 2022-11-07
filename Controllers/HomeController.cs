219d065a346494e5bc2902d63c7a1cf806f0fcf6
AmandaMariaFS <amandinhaferre@hotmail.com>
2022-11-06 22:46:01 -0300

    adcao da view doce

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,6 +38,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Doce()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
