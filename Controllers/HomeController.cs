bf07ed74bfdd96c6575293a5989c9447c93fa953
Ana Luisa Oliveira da Silva <analuisaoliveira714@gmail.com>
2022-11-21 19:58:38 -0300

    adicionando funcionalidades login e cadastro feitas em sala de aula

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,13 +12,13 @@ public class HomeController : Controller
     private static List<ReceitaViewModel> receitas = 
         new List<ReceitaViewModel>{
         new ReceitaViewModel(1, "Arroz negro à brasileira", "../imagens/arroznegro.jpg", "imagem de arroz negro à brasileira", "Salgado", "America", "https://www.youtube.com/embed/diKgUdmj0m8", "10 min", "2 porções", 0, 0),
-            new ReceitaViewModel(2, "Tartar de salmão, manga e avocado", "../imagens/tartar.jpg", "imagem de tartar de salmão, manga e avocado", "Salgado", "Africa", "https://youtu.be/OhiMLGrulKk", "10 min", "2 porções", 0, 0),
-            new ReceitaViewModel(3, "Torta de abóbora de noz e pecã", "../imagens/torta-abobora-nozes.png", "imagem de torta de abóbora de noz e pecã", "Doce", "Asia", "https://youtu.be/Pv6qx9nnBHg", "10 min", "2 porções", 0, 0),
-            new ReceitaViewModel(4, "Dakos", "../imagens/dakos.png", "imagem de dakos", "Salgado", "Europa", "https://youtu.be/fzBkeChLJL8", "10 min", "2 porções", 0, 0)
+            new ReceitaViewModel(2, "Tartar de salmão, manga e avocado", "../imagens/tartar.jpg", "imagem de tartar de salmão, manga e avocado", "Salgado", "Africa", "https://www.youtube.com/embed/OhiMLGrulKk", "10 min", "2 porções", 0, 0),
+            new ReceitaViewModel(3, "Torta de abóbora de noz e pecã", "../imagens/torta-abobora-nozes.png", "imagem de torta de abóbora de noz e pecã", "Doce", "Asia", "https://www.youtube.com/embed/Pv6qx9nnBHg", "10 min", "2 porções", 0, 0),
+            new ReceitaViewModel(4, "Dakos", "../imagens/dakos.png", "imagem de dakos", "Salgado", "Europa", "https://www.youtube.com/embed/fzBkeChLJL8", "10 min", "2 porções", 0, 0)
         };
 
     private static List<UsuarioViewModel> usuarios = new List<UsuarioViewModel>{
-        new UsuarioViewModel(1, "Amanda", "amandinhaferre@hotmail.com", "AmandaMariaFS", "EnolaHolmes")
+        new UsuarioViewModel("Amanda", "amandinhaferre@hotmail.com", "AmandaMariaFS", "EnolaHolmes")
     };
 
     bool cadastrado = false;
@@ -28,8 +28,10 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(string user)
     {   
+        ViewBag.user = user;
+        ViewBag.user = user;
         return View(receitas);
     }
 
@@ -38,14 +40,24 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Perfil()
+    public IActionResult Perfil(string user)
     {
+        foreach (var usuario in usuarios)
+        {
+            if (usuario.User == user)
+            {
+                ViewBag.user = user;
+                ViewBag.nome = usuario.Nome;
+                ViewBag.email = usuario.Email;
+            }
+        }
         return View();
     }
 
-    public IActionResult Salgado(string continente)
+    public IActionResult Salgado(string continente, string user)
     {
         ViewBag.continente = continente;
+        ViewBag.user = user;
         return View(receitas);
     }
 
@@ -59,23 +71,47 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Doce(string continente)
+    public IActionResult Doce(string continente, string user)
     {
         ViewBag.continente = continente;
+        ViewBag.user = user;
         return View(receitas);
     }
 
-    public IActionResult Search(string textBusca)
+    public IActionResult Search(string textBusca, string user)
     {
         ViewBag.textBusca = textBusca;
+        ViewBag.user = user;
         return View(receitas);
     }
 
-    public IActionResult Receita(int id)
+    public IActionResult Receita(int id, string user)
     {
         ViewBag.id = id;
+        ViewBag.user = user;
         return View(receitas);
     }
+
+    public IActionResult CadastroResultado(string nome, string email, string nomeusuario, string senha)
+    {
+        usuarios.Add(new UsuarioViewModel(nome, email, nomeusuario, senha));
+        ViewBag.user = nomeusuario;
+        return View();
+    }
+
+    public IActionResult LoginResultado(string email, string senha)
+    {
+        UsuarioViewModel user = new UsuarioViewModel();
+        foreach (var usuario in usuarios)
+        {
+            if (usuario.Email == email && usuario.Senha == senha)
+            {
+                user = usuario;
+            }
+        }
+
+        return View(user);
+    } 
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
--- ViewsModels/ReceitaViewModel.cs
+++ ViewsModels/ReceitaViewModel.cs
@@ -33,5 +33,5 @@ public class ReceitaViewModel
     public ReceitaViewModel()
     {
         
-    }
+    } 
 }
