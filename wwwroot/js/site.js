95793855dab2a6bd53fc9831d97705ade51ad962
Ana Luisa Oliveira da Silva <analuisaoliveira714@gmail.com>
2022-11-26 20:11:59 -0300

    adicionando função curtir

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const button = document.querySelector('#curtir');
+const numeroCurtidas = document.querySelector('#numeroCurtidas');
+const imagem =  document.querySelector('#coracao');
+
+button.addEventListener('click', () => {
+  let likeValue = document.querySelector('#numeroCurtidas').textContent;
+  let newValue = Number(likeValue) + 1;
+  button.classList.add('curtir');
+  numeroCurtidas.innerHTML = newValue;
+  imagem.setAttribute("src", "../imagens/favorito-preenchido.png");
+});
