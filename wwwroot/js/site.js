a8f9b3ab0768fc2ef4f584f7be056b6b104aa9f3
Ana Luisa Oliveira da Silva <analuisaoliveira714@gmail.com>
2022-11-26 20:22:55 -0300

    implementando funcionalidade avaliar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,6 +3,8 @@
 
 // Write your JavaScript code.
 
+
+//funcionalidade curtir
 const button = document.querySelector('#curtir');
 const numeroCurtidas = document.querySelector('#numeroCurtidas');
 const imagem =  document.querySelector('#coracao');
@@ -13,4 +15,19 @@ button.addEventListener('click', () => {
   button.classList.add('curtir');
   numeroCurtidas.innerHTML = newValue;
   imagem.setAttribute("src", "../imagens/favorito-preenchido.png");
+});
+
+
+//funcionalidade avaliar
+var stars = document.querySelectorAll('.star-icon');
+                  
+document.addEventListener('click', function(e){
+  var classStar = e.target.classList;
+  if(!classStar.contains('ativo')){
+    stars.forEach(function(star){
+      star.classList.remove('ativo');
+    });
+    classStar.add('ativo');
+    console.log(e.target.getAttribute('data-avaliacao'));
+  }
 });
