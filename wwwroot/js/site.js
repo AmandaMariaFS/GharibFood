d7d3aa884eda8a69d4a782393364323d896da218
Ana Luisa Oliveira da Silva <analuisaoliveira714@gmail.com>
2022-11-26 23:21:10 -0300

    alterando bug no avaliar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -46,8 +46,7 @@ function Avaliar(estrela) {
     var s5 = document.getElementById("s5").src;
     var avaliacao = 0;
    
-   if (estrela == 5){ 
-    if (s5 == url + "../imagens/star0.png") {
+   if (estrela == 6){ 
     document.getElementById("s1").src = "../imagens/star1.png";
     document.getElementById("s2").src = "../imagens/star1.png";
     document.getElementById("s3").src = "../imagens/star1.png";
@@ -55,13 +54,15 @@ function Avaliar(estrela) {
     document.getElementById("s5").src = "../imagens/star1.png";
     avaliacao = 5;
     } else {
-    document.getElementById("s1").src = "../imagens/star1.png";
-    document.getElementById("s2").src = "../imagens/star1.png";
-    document.getElementById("s3").src = "../imagens/star1.png";
-    document.getElementById("s4").src = "../imagens/star1.png";
-    document.getElementById("s5").src = "../imagens/star0.png";
-    avaliacao = 4;
-   }}
+    if (estrela == 5) {
+        document.getElementById("s1").src = "../imagens/star1.png";
+        document.getElementById("s2").src = "../imagens/star1.png";
+        document.getElementById("s3").src = "../imagens/star1.png";
+        document.getElementById("s4").src = "../imagens/star1.png";
+        document.getElementById("s5").src = "../imagens/star0.png";
+        avaliacao = 4;
+    }
+   }
     
     //ESTRELA 4
    if (estrela == 4){ 
