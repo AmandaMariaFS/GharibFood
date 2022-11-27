1c726cc39e468d7862d04407622b4b77233f72e4
Ana Luisa Oliveira da Silva <analuisaoliveira714@gmail.com>
2022-11-26 21:15:52 -0300

    acrescentando funcionalidade comentar com modal

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -31,3 +31,19 @@ document.addEventListener('click', function(e){
     console.log(e.target.getAttribute('data-avaliacao'));
   }
 });
+
+//funcionalidade comentar
+
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
